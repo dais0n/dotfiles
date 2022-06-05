feb06b59863fc5e5895a2e97bdce5ea4b4a7a185
dais0n <t.omura8383@gmail.com>
2022-06-05 12:17:23 +0900

    remove

--- rc/famous-saying.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/sh
-
-file="${HOME}/.zsh.d/famous-saying.json"
-num=$(cat ${file} | jq length | xargs -I{} expr $RANDOM % {})
-cat ${file} | jq -r '.['${num}'] | .result = .saying + " - " + .person | .result'
