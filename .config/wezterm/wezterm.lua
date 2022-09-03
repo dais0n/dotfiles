8f800d2a2890a135c069f8aa0688e596c7574a0e
dais0n <t.omura8383@gmail.com>
2022-09-03 13:45:51 +0900

    add wezterm

---
+++ .config/wezterm/wezterm.lua
@@ -0,0 +1,6 @@
+local wezterm = require 'wezterm'
+return {
+  font = wezterm.font("FiraMono Nerd Font Mono", {weight="Regular", stretch="Normal", style="Normal"}),
+  font_size = 13.0,
+  color_scheme = "nord",
+}
