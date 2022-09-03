342a8aa620306e5cb93d33f7edd8b6177135364e
dais0n <t.omura8383@gmail.com>
2022-09-03 06:40:27 +0000

    fix

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -1,9 +1,4 @@
 require('base')
 require('plugins')
 
-vim.g.nord_contrast = false
-vim.g.nord_borders = false
-vim.g.nord_disable_background = true
-vim.g.nord_italic = false
-vim.g.nord_uniform_diff_background = true
 vim.cmd([[colorscheme nord]])
