835b88368f765184b3f6c832207550108132b058
dais0n <t.omura8383@gmail.com>
2023-07-20 15:18:50 +0900

    fix

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -360,32 +360,16 @@ if require_obsidian then
 			folder = "dailies",
 			date_format = "%Y-%m-%d",
 		},
-		note_id_func = function(title)
-			-- Create note IDs in a Zettelkasten format with a timestamp and a suffix.
-			-- In this case a note with the title 'My new note' will given an ID that looks
-			-- like '1657296016-my-new-note', and therefore the file name '1657296016-my-new-note.md'
-			local suffix = ""
-			if title ~= nil then
-				-- If title is given, transform it into valid file name.
-				suffix = title:gsub(" ", "-"):gsub("[^A-Za-z0-9-]", ""):lower()
-			else
-				-- If title is nil, just add 4 random uppercase letters to the suffix.
-				for _ = 1, 4 do
-					suffix = suffix .. string.char(math.random(65, 90))
-				end
-			end
-			return tostring(os.time()) .. "-" .. suffix
-		end,
 	})
 end
 
 vim.keymap.set("n", "gf", function()
-	if require("obsidian").util.cursor_on_markdown_link() then
+	if obsidian.util.cursor_on_markdown_link() then
 		return "<cmd>ObsidianFollowLink<CR>"
 	else
 		return "gf"
 	end
-end, opts)
+end, { noremap = false, expr = true })
 
 -- general
 vim.opt.helplang = "ja,en"
