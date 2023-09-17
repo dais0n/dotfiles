8e3d50a19485490af4c3ea7b59529761f2fb6fde
dais0n <t.omura8383@gmail.com>
2023-09-17 05:32:07 +0000

    fix

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -66,6 +66,8 @@ require("lazy").setup({
   { "hrsh7th/cmp-cmdline", event = "InsertEnter" },
   { "williamboman/mason.nvim", cmd = { "Mason", "MasonInstall" } },
   { "j-hui/fidget.nvim", config = true, event = "LspAttach", tag = 'legacy'},
+  -- ruby
+  { "mogulla3/rspec.nvim", config = true, ft = 'ruby'},
   -- telescope
   {
     "nvim-telescope/telescope.nvim",
@@ -143,13 +145,46 @@ lspconfig.lua_ls.setup({
 lspconfig.gopls.setup({
   on_attach = on_attach,
   capabilities = capabilities,
+  settings = {
+    gopls = {
+      analyses = {
+        unusedparams = true,
+      },
+      -- staticcheck = true,
+      gofumpt = true,
+    },
+  },
 })
 
-lspconfig.sorbet.setup({
-  on_attach = on_attach,
-  capabilities = capabilities,
-  cmd = { "bundle", "exec", "srb", "tc", "--lsp" },
+-- goimports on save
+vim.api.nvim_create_autocmd("BufWritePre", {
+  pattern = "*.go",
+  callback = function()
+    local params = vim.lsp.util.make_range_params()
+    params.context = {only = {"source.organizeImports"}}
+    -- buf_request_sync defaults to a 1000ms timeout. Depending on your
+    -- machine and codebase, you may want longer. Add an additional
+    -- argument after params if you find that you have to write the file
+    -- twice for changes to be saved.
+    -- E.g., vim.lsp.buf_request_sync(0, "textDocument/codeAction", params, 3000)
+    local result = vim.lsp.buf_request_sync(0, "textDocument/codeAction", params)
+    for cid, res in pairs(result or {}) do
+      for _, r in pairs(res.result or {}) do
+        if r.edit then
+          local enc = (vim.lsp.get_client_by_id(cid) or {}).offset_encoding or "utf-16"
+          vim.lsp.util.apply_workspace_edit(r.edit, enc)
+        end
+      end
+    end
+    vim.lsp.buf.format({async = false})
+  end
 })
+
+-- lspconfig.sorbet.setup({
+--   on_attach = on_attach,
+--   capabilities = capabilities,
+--   cmd = { "bundle", "exec", "srb", "tc", "--lsp" },
+-- })
 
 lspconfig.tsserver.setup {
   on_attach = function(client, bufnr)
