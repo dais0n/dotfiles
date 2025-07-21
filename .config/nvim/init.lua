5aa0d7a3a1ac282ed46aa05383a6e10c87283483
dais0n <t.omura8383@gmail.com>
2025-07-21 23:54:49 +0900

    fix

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -1,4 +1,5 @@
 -- ref: https://github.com/nvim-lua/kickstart.nvim/blob/master/init.lua
+vim.loader.enable()
 vim.g.mapleader = ' '
 vim.g.maplocalleader = ' '
 vim.opt.number = true
@@ -6,6 +7,7 @@ vim.opt.breakindent = true
 vim.opt.undofile = true
 vim.opt.ignorecase = true
 vim.opt.smartcase = true
+vim.opt.wildignorecase = true
 vim.opt.updatetime = 250
 vim.opt.timeoutlen = 300
 vim.opt.splitright = true
@@ -16,7 +18,6 @@ vim.opt.inccommand = 'split' -- Preview substitutions live, as you type
 vim.opt.scrolloff = 10 -- Minimal number of screen lines to keep above and below the cursor.
 vim.opt.clipboard = "unnamedplus"
 vim.opt.swapfile = false
-vim.opt.pumblend = 10 -- popup window size
 vim.opt.termguicolors = true -- popup window color
 vim.opt.shiftwidth = 2
 vim.opt.tabstop = 2
@@ -26,16 +27,12 @@ vim.opt.smartindent = true
 vim.opt.mouse = 'a'
 vim.opt.autoread = true
 vim.opt.autowrite = true
-vim.opt.syntax = "enable"
 vim.opt.ttyfast = true
-vim.opt.synmaxcol = 2000
-vim.opt.modifiable = true
 
 vim.keymap.set('n', '<Esc>', '<cmd>nohlsearch<CR>') -- clear on pressing <Esc> in normal mode
-vim.api.nvim_create_autocmd({ "BufReadPost" }, { -- remember cursor position
-  pattern = { "*" },
+vim.api.nvim_create_autocmd('BufReadPost', {
   callback = function()
-    vim.api.nvim_exec('silent! normal! g`"zv', false)
+    pcall(function() vim.api.nvim_exec('silent! normal! g`"zv', false) end)
   end,
 })
 
@@ -47,11 +44,8 @@ vim.keymap.set('n', '<leader>q', vim.diagnostic.setloclist)
 
 -- Highlight when yanking (copying) text
 vim.api.nvim_create_autocmd('TextYankPost', {
-  desc = 'Highlight when yanking (copying) text',
   group = vim.api.nvim_create_augroup('kickstart-highlight-yank', { clear = true }),
-  callback = function()
-    vim.highlight.on_yank()
-  end,
+  callback = function() vim.highlight.on_yank() end,
 })
 
 vim.g.clipboard = {
@@ -82,9 +76,8 @@ vim.opt.rtp:prepend(lazypath)
 require("lazy").setup({
   { -- Fuzzy finder
     "folke/snacks.nvim",
-    lazy = false,
+    event = 'VeryLazy',
     priority = 1000,
-    ---@type snacks.Config
     opts = {
       indent    = { enabled = true },
       lazygit   = { enabled = true },
@@ -96,142 +89,30 @@ require("lazy").setup({
         layout = { preset = "bottom" },
       },
     },
-
     keys = {
-      { "<leader>gy", function() Snacks.gitbrowse() end },
-      { "<leader>lg", function() Snacks.lazygit()  end },
-      { "<leader>s.", function() Snacks.picker.recent() end },
-      { "<leader>sf", function() Snacks.picker.files() end },
-      { "<leader>sg", function() Snacks.picker.grep() end },
+      { "<leader>gy", function() require("snacks").gitbrowse() end },
+      { "<leader>lg", function() require("snacks").lazygit()  end },
+      { "<leader>s.", function() require("snacks").picker.recent() end },
+      { "<leader>sf", function() require("snacks").picker.files() end },
+      { "<leader>sg", function() require("snacks").picker.grep() end },
       { "<leader>sw", function()
           local word = vim.fn.expand("<cword>")
-          Snacks.picker.grep({ search = word })
+          require("snacks").picker.grep({ search = word })
         end
       },
-      { "<leader>sd", function() Snacks.picker.diagnostics() end },
-      { "<leader>sh", function() Snacks.picker.help()        end },
-      { "<leader>sk", function() Snacks.picker.keymaps()     end },
-      { "<leader><leader>", function() Snacks.picker.buffers() end },
-      { "<leader>b", function() Snacks.picker.lines({ cwd = false }) end},
+      { "<leader>sd", function() require("snacks").picker.diagnostics() end },
+      { "<leader>sh", function() require("snacks").picker.help()        end },
+      { "<leader>sk", function() require("snacks").picker.keymaps()     end },
+      { "<leader><leader>", function() require("snacks").picker.buffers() end },
+      { "<leader>b", function() require("snacks").picker.lines({ cwd = false }) end},
     },
   },
-  { -- LSP Configuration & Plugins
-    'neovim/nvim-lspconfig',
-    dependencies = {
-      { 'williamboman/mason.nvim',
-        cmd = {
-          "Mason",
-          "MasonInstall",
-          "MasonUninstall",
-          "MasonUninstallAll",
-          "MasonLog",
-          "MasonUpdate",
-        },
-      },
-      { "j-hui/fidget.nvim", tag = "v1.0.0", config = true },
-      { "aznhe21/actions-preview.nvim", event="LspAttach" },
-    },
-    config = function()
-      vim.api.nvim_create_autocmd('LspAttach', {
-        group = vim.api.nvim_create_augroup('kickstart-lsp-attach', { clear = true }),
-        callback = function(event)
-          local map = function(keys, func, desc)
-            vim.keymap.set('n', keys, func, { buffer = event.buf })
-          end
-          local p = Snacks.picker
-          map('gd',          p.lsp_definitions)
-          map('gr',          p.lsp_references)
-          map('gi',          p.lsp_implementations)
-          map('<leader>ld',   p.lsp_type_definitions)
-          map('<leader>ls',  p.lsp_symbols)
-          map('<leader>ws',  p.lsp_workspace_symbols)
-          map('<leader>ca', require("actions-preview").code_actions, '[C]ode [A]ction')
-          -- Opens a popup that displays documentation about the word under your cursor
-          map('K', vim.lsp.buf.hover, 'Hover Documentation')
-          local client = vim.lsp.get_client_by_id(event.data.client_id)
-          client.server_capabilities.document_formatting = true
-          if client and client.server_capabilities.documentHighlightProvider then
-            vim.api.nvim_create_autocmd({ 'CursorHold', 'CursorHoldI' }, {
-              buffer = event.buf,
-              callback = vim.lsp.buf.document_highlight,
-            })
-
-            vim.api.nvim_create_autocmd({ 'CursorMoved', 'CursorMovedI' }, {
-              buffer = event.buf,
-              callback = vim.lsp.buf.clear_references,
-            })
-          end
-        end,
-      })
-
-      require('mason').setup()
-
-      local capabilities = vim.lsp.protocol.make_client_capabilities()
-      capabilities = vim.tbl_deep_extend('force', capabilities, require('cmp_nvim_lsp').default_capabilities())
-
-      -- lua
-      require("lspconfig").lua_ls.setup({
-        capabilities = capabilities,
-        settings = {
-          Lua = {
-            diagnostics = {
-              globals = { "vim" },
-            },
-          },
-        },
-      })
-
-      -- typos
-      require("lspconfig").typos_lsp.setup({
-        init_options = {
-          config = '~/.config/typos/.typos.toml',
-        },
-      })
-
-      -- go
-      require('lspconfig').gopls.setup({
-        capabilities = capabilities,
-        settings = {
-          gopls = {
-            analyses = {
-              unusedparams = true,
-            },
-            gofumpt = true,
-          },
-        },
-      })
-      -- ref: https://github.com/golang/tools/blob/master/gopls/doc/vim.md#imports-and-formatting
-      vim.api.nvim_create_autocmd("BufWritePre", { -- goimports on save
-        pattern = "*.go",
-        callback = function()
-          local params = vim.lsp.util.make_range_params()
-          params.context = {only = {"source.organizeImports"}}
-          local result = vim.lsp.buf_request_sync(0, "textDocument/codeAction", params)
-          for cid, res in pairs(result or {}) do
-            for _, r in pairs(res.result or {}) do
-              if r.edit then
-                local enc = (vim.lsp.get_client_by_id(cid) or {}).offset_encoding or "utf-16"
-                vim.lsp.util.apply_workspace_edit(r.edit, enc)
-              end
-            end
-          end
-          vim.lsp.buf.format({async = false})
-        end
-      })
-
-      -- typescript
-      require("lspconfig").ts_ls.setup {
-        on_attach = function(client,_)
-          client.server_capabilities.documentFormattingProvider = false
-        end,
-        filetypes = {
-          'typescript',
-          'typescriptreact',
-          'typescript.tsx',
-        },
-      }
-    end,
+  {
+    'williamboman/mason.nvim',
+    cmd = { 'Mason', 'MasonInstall', 'MasonUninstall', 'MasonLog', 'MasonUpdate' },
+    config = true,
   },
+  { 'j-hui/fidget.nvim', tag = 'v1.0.0', event = 'LspAttach', config = true },
   { -- filer
     'stevearc/oil.nvim',
     opts = {},
@@ -241,7 +122,7 @@ require("lazy").setup({
           show_hidden = true,
         }
       })
-      vim.keymap.set("n", "<leader>o", ":Oil<CR>", { noremap = true, silent = true, desc = "[O]pen Current Dir" })
+      vim.keymap.set("n", "<leader>o", ":Oil<CR>", { noremap = true, silent = true})
     end
   },
   { 'vim-test/vim-test',
@@ -258,7 +139,7 @@ require("lazy").setup({
     dependencies = {
       { "neovim/nvim-lspconfig", event = "InsertEnter" },
       { "hrsh7th/cmp-buffer", event = "InsertEnter" },
-      { "hrsh7th/cmp-cmdline", event = "ModeChanged" },
+      { "hrsh7th/cmp-cmdline", event = "CmdlineEnter" },
       { "hrsh7th/cmp-nvim-lsp", event = "InsertEnter" },
       { "hrsh7th/cmp-nvim-lsp-document-symbol", event = "InsertEnter" },
       { "hrsh7th/cmp-nvim-lsp-signature-help", event = "InsertEnter" },
@@ -288,11 +169,9 @@ require("lazy").setup({
           { name = 'nvim_lsp' },
           { name = 'path' },
           { name = "buffer" },
-          { name = "path" },
           { name = "nvim_lsp_signature_help" },
           { name = 'treesitter' },
           { name = "cmdline" },
-          { name = 'render-markdown' },
         },
         formatting = {
           format = require("lspkind").cmp_format({
@@ -441,4 +320,85 @@ require("lazy").setup({
     )
   end
   },
+})
+
+require('mason').setup()
+
+local capabilities = vim.tbl_deep_extend('force',
+  vim.lsp.protocol.make_client_capabilities(),
+  require('cmp_nvim_lsp').default_capabilities()
+)
+
+local function on_attach(client, bufnr)
+  local map = function(keys, func, desc)
+    vim.keymap.set('n', keys, func, { buffer = bufnr, desc = desc })
+  end
+  map('gd', vim.lsp.buf.definition,        'Go to Definition')
+  map('gr', vim.lsp.buf.references,        'References')
+  map('gi', vim.lsp.buf.implementation,    'Implementations')
+  map('K',  vim.lsp.buf.hover,             'Hover')
+
+  if client.server_capabilities.documentHighlightProvider then
+    vim.api.nvim_create_autocmd({ 'CursorHold', 'CursorHoldI' }, {
+      buffer = bufnr,
+      callback = vim.lsp.buf.document_highlight,
+    })
+    vim.api.nvim_create_autocmd({ 'CursorMoved', 'CursorMovedI' }, {
+      buffer = bufnr,
+      callback = vim.lsp.buf.clear_references,
+    })
+  end
+end
+
+vim.lsp.config('*', { capabilities = capabilities, on_attach = on_attach })
+
+vim.lsp.config('lua_ls', {
+  cmd = { 'lua-language-server' },
+  filetypes = { 'lua' },
+  settings = { Lua = { diagnostics = { globals = { 'vim' } } } },
+})
+
+vim.lsp.config('gopls', {
+  cmd = { 'gopls' },
+  root_markers = { 'go.work', 'go.mod', '.git' },
+  settings = {
+    gopls = {
+      analyses = { unusedparams = true },
+      gofumpt   = true,
+    },
+  },
+})
+
+vim.lsp.config('tsserver', {
+  cmd = { 'typescript-language-server', '--stdio' },
+  filetypes = { 'typescript', 'typescriptreact', 'typescript.tsx' },
+  root_markers = { 'package.json', 'tsconfig.json', '.git' },
+  on_attach = function(client, bufnr)
+    client.server_capabilities.documentFormattingProvider = false
+    on_attach(client, bufnr)
+  end,
+})
+
+vim.lsp.config('typos_lsp', {
+  init_options = { config = vim.fn.expand('~/.config/typos/.typos.toml') },
+})
+
+vim.lsp.enable({ 'lua_ls', 'gopls', 'tsserver', 'typos_lsp' })
+
+vim.api.nvim_create_autocmd('BufWritePre', {
+  pattern = '*.go',
+  callback = function()
+    local params = vim.lsp.util.make_range_params()
+    params.context = { only = { 'source.organizeImports' } }
+    local result = vim.lsp.buf_request_sync(0, 'textDocument/codeAction', params)
+    for cid, res in pairs(result or {}) do
+      for _, r in pairs(res.result or {}) do
+        if r.edit then
+          local enc = (vim.lsp.get_client_by_id(cid) or {}).offset_encoding or 'utf-16'
+          vim.lsp.util.apply_workspace_edit(r.edit, enc)
+        end
+      end
+    end
+    vim.lsp.buf.format({ async = false })
+  end,
 })
